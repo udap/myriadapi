773e24c9d3cb5a47409701b8156160648016e09f
wangkun <kun.wang.bj@qq.com>
2020-06-13 22:45:35 +0800

    Revert "added RequestUser"
    This reverts commit a197889a3df92b8cae5b14655af266c6a5162089.

--- src/main/java/io/chainmind/myriadapi/MyriadApiApplication.java
+++ src/main/java/io/chainmind/myriadapi/MyriadApiApplication.java
@@ -1,28 +1,17 @@
 package io.chainmind.myriadapi;
 
-import io.chainmind.myriad.domain.RequestUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.context.annotation.RequestScope;
 
 @EnableCaching
 @EnableFeignClients
 @SpringBootApplication
 public class MyriadApiApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(MyriadApiApplication.class, args);
-    }
-
-    @Bean
-    @RequestScope
-    public RequestUser getCurrentUser() {
-        return new RequestUser();
     }
 
 }
