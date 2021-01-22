866fb09d0bb0a57b1d69059ae1faf99819bb0591
wangkun <kun.wang.bj@qq.com>
2021-01-22 16:35:02 +0800

    fig bug

--- src/main/java/io/chainmind/myriadapi/domain/dto/OrgDTO.java
+++ src/main/java/io/chainmind/myriadapi/domain/dto/OrgDTO.java
@@ -1,13 +1,14 @@
 package io.chainmind.myriadapi.domain.dto;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @EqualsAndHashCode
 @Builder
-public class OrgDTO {
+public class OrgDTO implements Comparable{
 	private String id;
 	private String name;
 	private String shortName;
@@ -15,4 +16,10 @@ public class OrgDTO {
 	private String licenseNo;
 	private String address;
 	private String phone;
+
+	@Override
+	public int compareTo(Object o) {
+		OrgDTO dto = (OrgDTO)o;
+		return this.id.compareTo(dto.getId());
+	}
 }
