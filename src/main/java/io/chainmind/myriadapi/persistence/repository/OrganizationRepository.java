737bf0ecf441759f96be3ab911a2d00787bf7763
cybertriton <li.zhangc@outlook.com>
2021-01-23 21:18:25 +0800

    revert back because front end not ready for the change

--- src/main/java/io/chainmind/myriadapi/persistence/repository/OrganizationRepository.java
+++ src/main/java/io/chainmind/myriadapi/persistence/repository/OrganizationRepository.java
@@ -13,27 +13,27 @@ public interface OrganizationRepository
 		extends JpaRepository<Organization, Long>, JpaSpecificationExecutor<Organization> {
 	
 	@Query("SELECT d FROM OrganizationClosure c join c.descendant d " +
-			"WHERE c.ancestor = :org and d.code = :code and d.status != 'DELETED'  ")
+			"WHERE c.ancestor = :org and d.code = :code and d.status = 'ACTIVE'  ")
 	Organization findDescendantByCode(@Param("org") Organization org,@Param("code")String code);
 
-	@Query("SELECT org  FROM Organization org WHERE org.licenseNo = :licenseNo AND org.status != 'DELETED' ")
+	@Query("SELECT org  FROM Organization org WHERE org.licenseNo = :licenseNo AND org.status = 'ACTIVE' ")
 	Organization findByLicenseNo(@Param("licenseNo")String licenseNo);
 
-	@Query("SELECT org  FROM Organization org WHERE org.upCode = :upCode AND org.status != 'DELETED'")
+	@Query("SELECT org  FROM Organization org WHERE org.upCode = :upCode AND org.status = 'ACTIVE'")
 	Organization findByUpCode(@Param("upCode")String upCode);
 
-	@Query("SELECT org  FROM Organization org WHERE org.wpCode = :wpCode AND org.status != 'DELETED'")
+	@Query("SELECT org  FROM Organization org WHERE org.wpCode = :wpCode AND org.status = 'ACTIVE'")
 	Organization findByWpCode(@Param("wpCode")String wpCode);
 
-	@Query("SELECT org  FROM Organization org WHERE org.apCode = :apCode AND org.status != 'DELETED' ")
+	@Query("SELECT org  FROM Organization org WHERE org.apCode = :apCode AND org.status = 'ACTIVE' ")
 	Organization findByApCode(@Param("apCode")String apCode);
 
 	@Query("SELECT o from Organization o JOIN OrganizationClosure c ON c.ancestor = o "
-			+ "WHERE c.descendant = :org and c.pathLength>=0 and o.parent is null and o.status != 'DELETED'")
+			+ "WHERE c.descendant = :org and c.pathLength>=0 and o.parent is null and o.status = 'ACTIVE'")
 	Organization findTopAncestor(@Param("org")Organization org);
 
 	@Query("SELECT d FROM OrganizationClosure c join c.descendant d "
-			+ "WHERE c.ancestor = :ancestor and c.descendant <> :ancestor")
+			+ "WHERE c.ancestor = :ancestor and c.descendant <> :ancestor and d.status = 'ACTIVE'")
 	List<Organization> findDescendants(@Param("ancestor")Organization ancestor);
 
 }
